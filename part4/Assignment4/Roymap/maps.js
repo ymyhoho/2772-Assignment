988ac22a820364c354d2b6f9d80d595ec91566ed
ymyhoho <31231076+ymyhoho@users.noreply.github.com>
2017-10-23 02:18:33 +1030

    almost

---
+++ part4/Assignment4/Roymap/maps.js
@@ -0,0 +1,38 @@
+
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: new google.maps.LatLng(-34.922763,138.603258),
+          mapTypeId: 'terrain'
+        });
+ 
+
+        var script = document.createElement('script');
+
+        script.src = 'maps.json';
+        document.getElementsByTagName('head')[0].appendChild(script);
+      }
+
+
+      window.eqfeed_callback = function(results) {
+        for (var i = 0; i < results.features.length; i++) {
+          var coords = results.features[i].geometry.coordinates;
+          var latLng = new google.maps.LatLng(coords[0],coords[1]);
+          var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+          });
+          var contentString = results.features[i].id;
+        }
+
+      }
+
+      var infoWindow = new google.map.InfoWindow({
+        content: contentString
+      })
+
+      marker.addListener('click', function(){
+        infoWindow.open(map, marker)
+      })
+    
