6a29b77f8ff4d2d4faf14e4dbac5f3035a556c5b
ymyhoho <31231076+ymyhoho@users.noreply.github.com>
2017-10-23 10:54:12 +1030

    done

--- part4/Assignment4/Roymap/maps.js
+++ part4/Assignment4/Roymap/maps.js
@@ -1,5 +1,6 @@
 
       var map;
+      //creating the map
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           zoom: 8,
@@ -7,7 +8,7 @@
           mapTypeId: 'terrain'
         });
  
-
+        //get location information from maps.json
         var script = document.createElement('script');
 
         script.src = 'maps.json';
@@ -15,20 +16,27 @@
         document.getElementsByTagName('head')[0].appendChild(script);
       }
 
-
-      window.eqfeed_callback = function addMarker(results) {
+      //creating a for loop to make the marker on map and get information from json at same time
+      window.feed_callback = function addMarker(results) {
         for (var i = 0; i < results.features.length; i++) {
+          //define the the coordinates
           var coords = results.features[i].geometry.coordinates;
           var latLng = new google.maps.LatLng(coords[0],coords[1]);
           var marker = new google.maps.Marker({position: latLng, map: map});
+          //get youtube video link
           var video = '<div><iframe width="300" height="200" src="https://www.youtube.com/embed/'
-                    +results.features[i].properties.InfoWindowUrl+'?rel=0" frameborder="0" allowfullscreen></iframe></div>';
-          var contentString = results.features[i].properties.place + results.features[i].properties.info + video + results.features[i].videoUrl;
-          var infoWindow = new google.maps.InfoWindow({content: contentString});
-          marker.addListener('click', function(){
-          infoWindow.open(map, this)});
+                    +results.features[i].properties.InfoWindowUrl+'?rel=0" frameborder="0" allowfullscreen></iframe></div>';          
+          var content = results.features[i].properties.place + results.features[i].properties.info + video ;
+          //display the information window when user click on the marker
+          var infowindow = new google.maps.InfoWindow();
+          google.maps.event.addListener(marker,'click', (function(marker, content){
+          return function(){
+            infowindow.setContent(content);
+            infowindow.open(map, marker);
+          };
+            })(marker, content));
         }
     
           
       }
-      
+      google.maps.event.addDomListener(window,'load',initialize);
